9410ce0c54db98abea1694f969b10f68c032d1dd
Pavel Serebryakov <cspk@list.ru>
2018-05-09 21:12:38 +0500

    repo: add i3status.sh
    Script viewing date, time, current layout, volume level and mpd status.

---
+++ i3status.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DATE="`LANG=ru_RU.utf8 date +'%d %b %a %H:%M:%S'`"
+LANG="`xkblayout-state print %s`"
+
+VOL_STRING="`amixer get Master | grep % | sed '2d'`"
+VOL_PERCENT="`echo "$VOL_STRING" | grep -Eo '[0-9]{1,3}\%'`"
+
+VOL_STATE="`echo "$VOL_STRING" | grep -Eo "(\[on\]|\[off\])" | tr -d []`"
+if [ "$VOL_STATE" = "off" ]; then
+    VOL_CHAR="🔇"
+else
+    VOL_CHAR="🔊"
+fi
+
+VOL="$VOL_CHAR $VOL_PERCENT"
+
+MPD_STATUS="`mpc status`"
+MPD_RC=$?
+MPD_STATE="`echo $MPD_STATUS | grep -Eo '\[.*\]' | tr -d "[]"`"
+if [ $MPD_RC -eq 1 ]; then
+    MPD="mpd not running |"
+elif [ -z "$MPD_STATE" ]; then
+    MPD=""
+elif [ "$MPD_STATE" = "paused" ]; then
+    MPD="⏸ `mpc current` |"
+else
+    MPD="▶ `mpc current` |"
+fi
+
+echo "$MPD $VOL | $LANG | $DATE"
