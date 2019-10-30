8e8d559a45f28f5ca2c5d107cc0559c6d7cd6d43
Pavel Serebryakov <serebryakovp@progressufa.ru>
2019-10-30 14:05:44 +0500

    i3status.sh: use i3status

--- i3status.sh
+++ i3status.sh
@@ -1,34 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 
-DATE="`LANG=en_US.UTF-8 date +'%a %b %d %H:%M:%S'`"
-LANG="`xkblayout-state print %s`"
+i3status | while :
+do
+	read line
 
-VOL_STRING="`amixer get Master | grep % | sed '2d'`"
-VOL_PERCENT="`echo "$VOL_STRING" | grep -Eo '[0-9]{1,3}\%' | tr -d \%`"
+	VOL=`echo $line | cut -d '|' -f 1`
+	DATE=`echo $line | cut -d '|' -f 2`
+	LANG="`xkblayout-state print %s`"
 
-VOL_STATE="`echo "$VOL_STRING" | grep -Eo "(\[on\]|\[off\])" | tr -d []`"
-if [ "$VOL_STATE" = "off" ]; then
-	VOL_CHAR="vol muted"
-else
-	VOL_CHAR="vol"
-fi
+	MPD_STATUS="`mpc status | sed '1d;3d'`"
+	MPD_CURRENT="`mpc current`"
+	MPD_RC=$?
+	MPD_STATE="`echo "$MPD_STATUS" | grep -Eo '\[.*\]' | tr -d "[]"`"
+	MPD_TIME="`echo "$MPD_STATUS" | awk '{ print $3 }'`"
+	MPD_TRACKS="`echo "$MPD_STATUS" | awk '{ print $2 }' | tr -d '#'`"
+	if [ $MPD_RC -eq 1 ]; then
+		MPD="mpd not running |"
+	elif [ -z "$MPD_STATE" ]; then
+		MPD=""
+	elif [ "$MPD_STATE" = "paused" ]; then
+		MPD="[paused] `mpc current` ($MPD_TIME) [$MPD_TRACKS] |"
+	else
+		MPD="[playing] `mpc current` ($MPD_TIME) [$MPD_TRACKS] |"
+	fi
 
-VOL="$VOL_CHAR $VOL_PERCENT%"
-
-MPD_STATUS="`mpc status | sed '1d;3d'`"
-MPD_CURRENT="`mpc current`"
-MPD_RC=$?
-MPD_STATE="`echo "$MPD_STATUS" | grep -Eo '\[.*\]' | tr -d "[]"`"
-MPD_TIME="`echo "$MPD_STATUS" | awk '{ print $3 }'`"
-MPD_TRACKS="`echo "$MPD_STATUS" | awk '{ print $2 }' | tr -d '#'`"
-if [ $MPD_RC -eq 1 ]; then
-	MPD="mpd not running |"
-elif [ -z "$MPD_STATE" ]; then
-	MPD=""
-elif [ "$MPD_STATE" = "paused" ]; then
-	MPD="[paused] `mpc current` ($MPD_TIME) [$MPD_TRACKS] |"
-else
-	MPD="[playing] `mpc current` ($MPD_TIME) [$MPD_TRACKS] |"
-fi
-
-echo "$MPD $VOL | $LANG | $DATE "
+	echo "$MPD $VOL| $LANG |$DATE " || exit 1
+done
